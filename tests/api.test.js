51ea976afdf61b280b99cb14d536f1d10e3f4635
Ben Schoon <benny@dal.ca>
2026-03-06 13:22:27 -0400

    Updated Api tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,15 +1,15 @@
 import request from "supertest";
 import { handler } from "../netlify/functions/api.js";
 
-describe("Recommendation Api", () => {
+describe("Search Api", () => {
     it("should return artist data for an artist search", async () => {
-        const res = await request(handler).get("/api/artists?name=beatles");
+        const res = await request(handler).get("/api/search_artist?name=beatles");
         expect(res.statusCode).toBe(200);
         expect(res.body[0]).toHaveProperty("name");
     });
-    it("should return an error for missing city", async () => {
-        const res = await request(handler).get("/api/weather");
+    it("should return an error for missing name", async () => {
+        const res = await request(handler).get("/api/search_artist");
         expect(res.statusCode).toBe(400);
-        expect(res.body.error).toBe("City is required");
+        expect(res.body.error).toBe("Missing 'artistName' url param");
     });
 });
